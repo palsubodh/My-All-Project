fa986bf4881931ae2252fe8fa5c7ba362288a525
palsubodh <palsubodh390@gmail.com>
2022-05-30 16:54:17 +0530

    Evalution c-2

---
+++ Evalution c-2/scripts/checkout.js
@@ -0,0 +1,18 @@
+document.querySelector("confirm").addEventListener("click",cheackout)
+
+function cheackout(){
+    alert("Your order is accepted ")
+    setTimeout(function(){
+        alert(" Your order is being Prepared ")
+    },3000)
+    setTimeout(function(){
+        alert("Your order is being packed  ")
+    },8000)
+    setTimeout(function(){
+        alert(" Your order is out for delivery ")
+    },10000)
+    setTimeout(function(){
+        alert("Order delivered")
+    },12000)
+}
+    
